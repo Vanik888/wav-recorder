524c64a7cbe709a86fac783a42253deae168091e
Vanik Karslyan <v.karslyan@corp.mail.ru>
2016-04-29 17:04:05 +0300

    Добавил пункты меню и картинки

--- analyzer/commands_dictionary/menu.py
+++ analyzer/commands_dictionary/menu.py
@@ -16,6 +16,34 @@ menu = {
             'description': "c шоколадом и мороженым",
             'image_path': './stat/inteface_images/krep_klubnichno_orekhoviy.png'
         },
+        'EscimoFondu' : {
+            'name': "Эскимо и фондю",
+            'price': 350,
+            'count': 0,
+            'description': "сливочное эскимо с хрустящими посыпками",
+            'image_path': './stat/inteface_images/escimo_fondu.png'
+        },
+        'PizzaTuttyFrutty' : {
+            'name': "Пицца Тутти Фрутти",
+            'price': 650,
+            'count': 0,
+            'description': "с Тоффи соусом и ягодами",
+            'image_path': './stat/inteface_images/pizza_tutty_frutty.png'
+        },
+        'ChocoloateHurt' : {
+            'name': "Фондан Тающее Шоколадное Сердце",
+            'price': 350,
+            'count': 0,
+            'description': "с белым шоколадом, мороженым и ягодами",
+            'image_path': './stat/inteface_images/serdce_chocolat.png'
+        },
+        'KrepWithEggs' : {
+            'name': "Креп с яйцом и беконом",
+            'price': 199,
+            'count': 0,
+            'description': "c соусом из шпината и моцареллой",
+            'image_path': './stat/inteface_images/krep_s_bekonom.png'
+        },
     },
     'drinks': {
         'Espresso' : {
@@ -23,42 +51,42 @@ menu = {
             'price': 100,
             'count': 0,
             'description': "Горячий кофе",
-            'image_path': './stat/inteface_images/tutty_frutty_400_300.png'
+            'image_path': './stat/inteface_images/espresso.png'
         },
-        'Americano' : {
+        'Amevricano' : {
             'name': "Американо",
             'price': 165,
             'count': 0,
             'description': "Горячий кофе",
-            'image_path': './stat/inteface_images/krep_klubnichno_orekhoviy.png'
+            'image_path': './stat/inteface_images/americano.png'
         },
         'Maciato' : {
             'name': "Макиато",
             'price': 130,
             'count': 0,
             'description': "Горячий кофе",
-            'image_path': './stat/inteface_images/tutty_frutty_400_300.png'
+            'image_path': './stat/inteface_images/maciato.png'
         },
         'Kapuchino' : {
             'name': "Капучино",
             'price': 210,
             'count': 0,
             'description': "Горячий кофе",
-            'image_path': './stat/inteface_images/krep_klubnichno_orekhoviy.png'
+            'image_path': './stat/inteface_images/capucino.png'
         },
         'Latte' : {
             'name': "Латте",
             'price': 240,
             'count': 0,
             'description': "Горячий кофе",
-            'image_path': './stat/inteface_images/krep_klubnichno_orekhoviy.png'
+            'image_path': './stat/inteface_images/latte.png'
         },
         'IceLatte' : {
             'name': "Айс лате",
             'price': 240,
             'count': 0,
             'description': "Кофе с мороженым",
-            'image_path': './stat/inteface_images/tutty_frutty_400_300.png'
+            'image_path': './stat/inteface_images/icelatte.png'
         },
 
         'Tea' : {
@@ -66,14 +94,14 @@ menu = {
             'price': 230,
             'count': 0,
             'description': "Чай",
-            'image_path': './stat/inteface_images/krep_klubnichno_orekhoviy.png'
+            'image_path': './stat/inteface_images/tea_cup.png'
         },
         'WhiteChocolateTea' : {
             'name': "Белый шоколадный чай",
             'price': 270,
             'count': 0,
             'description': "Чай с ароматом шоколада",
-            'image_path': './stat/inteface_images/tutty_frutty_400_300.png'
+            'image_path': './stat/inteface_images/white_chocolate_tea.png'
         },
     },
     'services': {
@@ -82,42 +110,42 @@ menu = {
             'price': 0,
             'count': 0,
             'description': "Салфетка для рук",
-            'image_path': './stat/inteface_images/krep_klubnichno_orekhoviy.png'
+            'image_path': './stat/inteface_images/napkins.png'
         },
         'Fork' : {
             'name': "Вилка",
             'price': 0,
             'count': 0,
             'description': "Принести вилку",
-            'image_path': './stat/inteface_images/tutty_frutty_400_300.png'
+            'image_path': './stat/inteface_images/fork_steel.png'
         },
         'Spoon' : {
             'name': "Чайная ложка",
             'price': 0,
             'count': 0,
             'description': "Принести чайную ложку",
-            'image_path': './stat/inteface_images/tutty_frutty_400_300.png'
+            'image_path': './stat/inteface_images/spoon.png'
         },
         'Tablespoon' : {
             'name': "Столовая ложка",
             'price': 0,
             'count': 0,
             'description': "Принести столовую ложку",
-            'image_path': './stat/inteface_images/krep_klubnichno_orekhoviy.png'
+            'image_path': './stat/inteface_images/table_spoon.png'
         },
         'Knife' : {
             'name': "Ножик",
             'price': 0,
             'count': 0,
             'description': "Принести ножик",
-            'image_path': './stat/inteface_images/krep_klubnichno_orekhoviy.png'
+            'image_path': './stat/inteface_images/knife.png'
         },
         'Waiter' : {
             'name': "Официант",
             'price': 0,
             'count': 0,
             'description': "Вызов официанта",
-            'image_path': './stat/inteface_images/tutty_frutty_400_300.png'
+            'image_path': './stat/inteface_images/waiter.png'
         },
     },
 
